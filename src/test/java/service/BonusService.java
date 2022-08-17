6ead66c9506ded411b64399965b29c4f6ea9d361
dineysd12 <diney.sd1@gmail.com>
2022-08-16 22:54:55 -0300

    deveriaBonusSerDe10PorcentoParaSalarioIgualA10Mil

--- src/test/java/service/BonusService.java
+++ src/test/java/service/BonusService.java
@@ -11,7 +11,7 @@ import br.com.alura.tdd.modelo.Funcionario;
 
 public class BonusService {
     @Test
-    public void deveriaBonusSerZeroParaSalarioMuitoAlto() {
+    public void deveriaBonusSerZeroParaSalarioAcimaDe10Mil() {
         br.com.alura.tdd.service.BonusService serviceb = new br.com.alura.tdd.service.BonusService();
         BigDecimal bonus = serviceb.calcularBonus(new Funcionario("nome", LocalDate.now(), new BigDecimal("12000")));
 
@@ -19,10 +19,18 @@ public class BonusService {
     }
 
     @Test
-    public void deveriaBonusSerDe10PorcentoDoSalario() {
+    public void deveriaBonusSerDe10PorcentoParaSalarioAbaixoDe10Mil() {
         br.com.alura.tdd.service.BonusService serviceb = new br.com.alura.tdd.service.BonusService();
         BigDecimal bonus = serviceb.calcularBonus(new Funcionario("nome", LocalDate.now(), new BigDecimal("2500")));
 
         assertEquals(new BigDecimal("250.00"), bonus);
+    }
+
+    @Test
+    public void deveriaBonusSerDe10PorcentoParaSalarioIgualA10Mil() {
+        br.com.alura.tdd.service.BonusService serviceb = new br.com.alura.tdd.service.BonusService();
+        BigDecimal bonus = serviceb.calcularBonus(new Funcionario("nome", LocalDate.now(), new BigDecimal("10000")));
+
+        assertEquals(new BigDecimal("1000.00"), bonus);
     }
 }
